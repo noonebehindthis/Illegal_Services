3ecb3e63ce0d2b075bd287d0160d8a8fb9a01b36
Illegal-Services <BUZZARDGTA@protonmail.com>
2022-06-13 14:55:59 +0200

    website

--- Website_Builder.bat
+++ Website_Builder.bat
@@ -9,6 +9,7 @@ if defined href_path (
     set href_path=
 )
 set "sp= "
+set /a counter_links=0
 if exist "Bookmarks Toolbar\" (
     rd /s /q "Bookmarks Toolbar"
 )
@@ -52,6 +53,7 @@ for /f "tokens=1,2*" %%A in ('python "D:\Téléchargements\Python Stuff\IS.bookm
                     )
                     echo "[%%A] [!href_path!] [!name!]"
                 ) else if "%%A"=="LINK" (
+                    set /a counter_links+=1
                     set "href_link=%%F"
                     >>"!href_path!/index.html" (
                         echo         ^<a href="!href_link!" target="_blank"^>^<i class="fa fa-globe"^>^</i^> !name!^</a^>
@@ -65,6 +67,11 @@ for /f "tokens=1,2*" %%A in ('python "D:\Téléchargements\Python Stuff\IS.bookm
             )
         )
     )
+)
+call :GET_CURRENT_DATE || (
+    echo ERROR ^(GET_DATE_TIME^)
+    pause
+    exit /b 1
 )
 for /f "delims=" %%A in ('2^>nul dir "Bookmarks Toolbar\*index.html" /b /a:-d /s') do (
     set "href_path=%%~dpA"
@@ -148,6 +155,11 @@ exit /b
     echo     ^</div^>
     echo     ^<br^>
     echo:
+    echo     ^<div class="counter"^>
+    echo         Updated: 13/06/2022^&nbsp;^&nbsp;^|^&nbsp;^&nbsp;!counter_links! links indexed.
+    echo     ^</div^>
+    echo     ^<br^>
+    echo:
     echo     ^<footer^>
     echo         ^<a href="https://illegal-services.github.io/Illegal_Services/"^>^<img src="/Illegal_Services/svgs/internet.svg" title="https://illegal-services.github.io/Illegal_Services/" target="_blank"^>^</a^>
     echo         ^<a href="https://github.com/Illegal-Services/Illegal_Services"^>^<img src="/Illegal_Services/svgs/github.svg" title="https://github.com/Illegal-Services/Illegal_Services" target="_blank"^>^</a^>
@@ -165,3 +177,99 @@ exit /b
     exit /b 0
 )
 exit /b
+
+:GET_CURRENT_DATE
+if defined current_date (
+    set current_date=
+)
+for /f "tokens=2delims==." %%A in ('2^>nul wmic os get Localdatetime /value') do (
+    set "current_date=%%A"
+    set "current_date=!current_date:~-8,2!/!current_date:~-10,2!/!current_date:~0,-10!"
+    goto :L1
+)
+:L1
+call :CHECK_CURRENT_DATE current_date && (
+    exit /b 0
+)
+if defined current_date (
+    set current_date=
+)
+>nul chcp 437
+for /f "delims=" %%A in ('2^>nul powershell get-date -format "{dd/MM/yyyy}"') do (
+    set "current_date=%%A"
+    goto :L2
+)
+:L2
+>nul chcp 65001
+call :CHECK_CURRENT_DATE current_date && (
+    exit /b 0
+)
+exit /b 1
+
+:CHECK_CURRENT_DATE
+if not defined %1 (
+    exit /b 1
+)
+if not "!%1:~2,1!"=="/" (
+    exit /b 1
+)
+if not "!%1:~5,1!"=="/" (
+    exit /b 1
+)
+for /f "tokens=1-3delims=/" %%A in ("!%1!") do (
+    set "x=%%C"
+    call :CHECK_NUMBER x && (
+        if "%%B"=="01" (
+            set y1=31
+        ) else if "%%B"=="02" (
+            set "years=%%C"
+            call :IS_LEAP_YEAR_OR_NOT
+            if !leap!==1 (
+                set y1=29
+            ) else (
+                set y1=28
+            )
+        ) else if "%%B"=="03" (
+            set y1=31
+        ) else if "%%B"=="04" (
+            set y1=30
+        ) else if "%%B"=="05" (
+            set y1=31
+        ) else if "%%B"=="06" (
+            set y1=30
+        ) else if "%%B"=="07" (
+            set y1=31
+        ) else if "%%B"=="08" (
+            set y1=31
+        ) else if "%%B"=="09" (
+            set y1=30
+        ) else if "%%B"=="10" (
+            set y1=31
+        ) else if "%%B"=="11" (
+            set y1=30
+        ) else if "%%B"=="12" (
+            set y1=31
+        )
+        if defined y1 (
+            set "x=%%A"
+            call :CHECK_NUMBER_BETWEEN_CUSTOM x 01-!y1! && (
+                exit /b 0
+            )
+        )
+    )
+)
+exit /b 1
+
+:CHECK_NUMBER
+if not defined %1 exit /b 1
+for /f "delims=0123456789" %%A in ("!%1!") do exit /b 1
+exit /b 0
+
+:CHECK_NUMBER_BETWEEN_CUSTOM
+if not defined %1 exit /b 1
+for /f "delims=0123456789" %%A in ("!%1!") do exit /b 1
+for /f "tokens=1,2delims=-" %%A in ("%~2") do (
+    if !%1! lss %%A exit /b 1
+    if !%1! gtr %%B exit /b 1
+)
+exit /b 0
