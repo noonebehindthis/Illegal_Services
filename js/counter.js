9985ce4a8041c680495e035eb9f96896d894c584
Illegal-Services <BUZZARDGTA@protonmail.com>
2022-06-22 13:23:04 +0200

    Update IS.bookmarks.html

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,6 @@
 document.write(`
     <div class="counter">
-        Updated: 21/06/2022&nbsp;&nbsp;|&nbsp;&nbsp;3279 links indexed.
+        Updated: 22/06/2022&nbsp;&nbsp;|&nbsp;&nbsp;3873 links indexed.
     </div>
     <br>
 `);
